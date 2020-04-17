68cc08fcb503d2a79df4f6738541f23fb1866fec
Nathanael Birri <nathichuta@gmail.com>
2020-04-17 15:39:48 -0500

    Added user search functionality backend

--- Server/routes/user.router.js
+++ Server/routes/user.router.js
@@ -4,7 +4,7 @@ const
     { userController } = require(path.join(__dirname, '..', 'controllers'));
 router.post("/change-pic", userController.changeProfilePic)
 router.get('/feetch-feeds', userController.fetchFeed);
-router.post("/create-post",userController.createPost);
+router.post("/create-post", userController.createPost);
 router.get("/posts", userController.getPosts);
 router.get("/posts/:p_Id", userController.updatepostget);
 router.post("/updatepost", userController.updatepost);
@@ -16,14 +16,14 @@ router.get("/updateComment/:p_Id", userController.updatecommentget);
 router.post("/updateComment", userController.updatecomment);
 router.post("/deleteComment/:d_cid", userController.deletecomment);
 router.post("/search-post", userController.searchposts);
-router.post('/:id', userController.updateUser)
-router.post("/follow/:uid", userController.followUser );
+router.post("/follow/:uid", userController.followUser);
 router.post("/unfollow/:uid", userController.unFollowUser)
 router.post("/posts", userController.getAllPosts);
 router.get("/followings", userController.getFollowings);
 router.get("/followers", userController.getFollowers);
 router.get("/:id", userController.getUser);
 router.get("/", userController.getAllUsers);
+router.post("/search-posts", userController.searchPosts);
 
 
 
