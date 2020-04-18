47ead7b4d0a74cfc976dc48270a75f78e716a36e
Nathanael Birri <nathichuta@gmail.com>
2020-04-18 16:36:37 -0500

    Admin home component user service integration

--- Client/src/app/pages/admin/admin-home/admin-home.component.ts
+++ Client/src/app/pages/admin/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services';
 
 @Component({
   selector: 'app-admin-home',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    this.userService.connect();
+    this.userService.adminBadPostSubject.subscribe(res => {
+      console.log("POSTED");
+    })
+  }
 
   ngOnInit(): void {
   }
