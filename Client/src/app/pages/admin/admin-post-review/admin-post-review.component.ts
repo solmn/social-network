a1b03c7dc6863705160a4fe62894b4157e265773
Nathanael Birri <nathichuta@gmail.com>
2020-04-19 17:06:10 -0500

    Accept reject, bad post  functional

--- Client/src/app/pages/admin/admin-post-review/admin-post-review.component.ts
+++ Client/src/app/pages/admin/admin-post-review/admin-post-review.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/services/admin.service';
 import { first } from 'rxjs/operators';
 import { UserService } from 'src/app/services';
+import { AdminHomeComponent } from '../admin-home/admin-home.component';
 
 @Component({
   selector: 'app-admin-post-review',
@@ -11,38 +12,75 @@ import { UserService } from 'src/app/services';
 export class AdminPostReviewComponent implements OnInit {
   adminNotifications:any
   allBadPosts:any
-  constructor(private adminService:AdminService, private userService: UserService) { 
-    this.allBadPosts = adminService.getBadWordPostData();
+  constructor(
+    private adminService:AdminService, 
+    private userService: UserService, 
+    private adminHomeComponent: AdminHomeComponent) { 
+
     this.userService.adminBadPostSubject.subscribe(res => {
-      console.log("BAD bad bad word posted");
+      this.getFlaggedPosts(); 
+      console.log("BAD bad bad word posted",res);
       
     })
+    
   }
 
   ngOnInit(): void {
+    this.getFlaggedPosts();
   }
-
-  acceptBadPost(badPost,index){
-    this.adminService.approvePost(badPost)
+getFlaggedPosts(){
+  this.adminService.getBadWordedPosts().pipe(first())
+                   .subscribe(response=>{
+                    this.allBadPosts = response.result; 
+                    console.log("GOT THE POSTS FETACHED........",this.allBadPosts)
+  })
+  
+}
+acceptBadPost(badPost,index){
+    this.adminService.approvePost({notiId: badPost._id,postId: badPost.post._id })
                      .pipe(first())
                      .subscribe(response=>{
-                      this.allBadPosts[index] = response.result;
+                      let index = this.allBadPosts.findIndex(b => b.post._id == badPost.post._id );
+                      if(index !=-1) {
+                        this.allBadPosts.splice(index, 1);
+                      }
+                      // this.getFlaggedPosts();
+                      // //re-load all bad post words again
                      });
     
   }
 
-  rejectBadPost(badPost ,index){
-    this.adminService.rejectPost(badPost)
+rejectBadPost(badPost ,index){
+  console.log(badPost, index);
+    if(badPost.user.badPostCount+1>20){
+      this.deactivateAccount(badPost.user._id, badPost.post._id, badPost._id);
+    }
+    else{
+      this.adminService.rejectPost({notiId: badPost._id, userId:badPost.user._id, postId: badPost.post._id })
                      .pipe(first())
                      .subscribe(response=>{
-                          this.allBadPosts[index] = response.result;
-                  
+                      let index = this.allBadPosts.findIndex(b => b.post._id == badPost.post._id );
+                      if(index !=-1) {
+                        this.allBadPosts.splice(index, 1);
+                      }
+                      //this.getFlaggedPosts();
                       });
   }
+}
 
-
-
-
+deactivateAccount(userId, postId, notiId){
+  console.log("About to deactivated", userId)
+  this.adminService.deactivateAccount({userId: userId, postId:postId, notiId})
+          .pipe(first())
+          .subscribe(response=>{
+            let index = this.allBadPosts.findIndex(b => b.post._id == postId);
+            if(index !=-1) {
+              this.allBadPosts.splice(index, 1);
+            }
+                console.log("Account has deactivated");
+            //deactivated account  
+          })
+}
 
 
 }
