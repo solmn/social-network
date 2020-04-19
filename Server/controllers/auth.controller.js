91206bc7def698bf851a43f916b13c97c4963daa
Nathanael Birri <nathichuta@gmail.com>
2020-04-19 17:09:31 -0500

    Activate account conformation mailer

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,48 +1,56 @@
-const 
+const
     path = require('path'),
     { ApiResponse } = require(path.join(__dirname, "..", "util")),
     { authService } = require(path.join(__dirname, '..', 'services'));
 
-exports.register =  async (req, res, next) => {
-    try{
+exports.register = async(req, res, next) => {
+    try {
         let response = await authService.signup(req.body);
         res.status(response.status).json(response);
-    }catch(err) {
+    } catch (err) {
         res.status(500).json(new ApiResponse(500, 'error', err));
     }
 
 }
-exports.login = async (req, res, next) => {
+exports.login = async(req, res, next) => {
     try {
         let response = await authService.login(req.body.username, req.body.password);
         res.status(response.status).json(response);
-    }catch(err) {
+    } catch (err) {
         res.status(500).json(new ApiResponse(500, 'error', err));
     }
 }
 
-exports.forgotPassword = async (req, res, next) => {
-    try{
-        if(!req.body.email) {
-            res.status(500).json(new ApiResponse(500, 'error', {err: "Email required"}));
+exports.forgotPassword = async(req, res, next) => {
+    try {
+        if (!req.body.email) {
+            res.status(500).json(new ApiResponse(500, 'error', { err: "Email required" }));
         }
-       let response  = await authService.forgotPassword(req.body.email);
-       res.status(response.status).json(response);
-    }
-    catch(err) {
+        let response = await authService.forgotPassword(req.body.email);
+        res.status(response.status).json(response);
+    } catch (err) {
         console.log(err);
         res.status(500).json(new ApiResponse(501, 'error', err));
     }
-    
+
 }
 
-exports.resetPassword  = async (req, res, next) => {
-    try{
-       let response= await authService.resetPassword(req.body.token, req.body.password);
-       res.status(response.status).json(response);
-    }catch(err) {
+exports.resetPassword = async(req, res, next) => {
+    try {
+        let response = await authService.resetPassword(req.body.token, req.body.password);
+        res.status(response.status).json(response);
+    } catch (err) {
         console.log(err);
         res.status(500).json(new ApiResponse(500, 'error', err));
     }
 }
 
+exports.activateAccount = async(req, res, next) => {
+    try {
+        let response = await authService.activateAccount(req.body.email);
+        res.status(response.status).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(new ApiResponse(500, 'error', err));
+    }
+}
